7e5aff424acffbd8e9695ba1b2ecc60a1a7c3d1e
haohui.ding <haohui.ding@shebaochina.com>
2020-04-09 17:41:01 +0800

    spriongboot邮件服务

---
+++ sb_mail_server/src/test/java/com/dhh/sb_mail_server/SbMailServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhh.sb_mail_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMailServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
